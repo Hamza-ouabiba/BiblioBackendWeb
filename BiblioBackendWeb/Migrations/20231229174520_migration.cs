293eb564f4d06b59a2db1350d7ac358284dedda2
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2024-01-03 13:14:44 +0100

    UPDATE

---
+++ BiblioBackendWeb/Migrations/20231229174520_migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BiblioApp.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Employe",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "genre",
+                table: "Employe",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Employe");
+
+            migrationBuilder.DropColumn(
+                name: "genre",
+                table: "Employe");
+
+        }
+    }
+}
---
+++ BiblioBackendWeb/Migrations/20231230200948_migration6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BiblioApp.Migrations
+{
+    public partial class migration6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "periode",
+                table: "Reservation",
+                newName: "dateFin");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateDebut",
+                table: "Reservation",
+                type: "date",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dateDebut",
+                table: "Reservation");
+
+            migrationBuilder.RenameColumn(
+                name: "dateFin",
+                table: "Reservation",
+                newName: "periode");
+        }
+    }
+}
